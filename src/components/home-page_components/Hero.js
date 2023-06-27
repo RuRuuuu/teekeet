6204253125181195e7049a8eb8f938f7b066e41c
RuRuuuu <ayokooghenerukevwe2222@gmail.com>
2023-06-27 20:10:13 +0100

    login state corrected

--- src/components/home-page_components/Hero.js
+++ src/components/home-page_components/Hero.js
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Background from "../../assets/homepage_images/background.svg";
-import { Link } from "react-router-dom"
+
 function Hero() {
+  const [isloggedIn, setIsLoggedIn] = useState("Already logged in");
+
   return (
     <div
       style={{
         backgroundImage: `linear-gradient(180.01deg, #030A2E 0.01%, rgba(5, 12, 47, 0) 141.96%), 
       url(${Background})`,
         backgroundSize: "cover",
-        backgroundRepeat: "no-repeat"
+        backgroundRepeat: "no-repeat",
       }}
-      className="lg:py-[5.5rem] lg:h-[30rem] md:rounded-b-[80px] w-full rounded-b-[50px] py-[4rem] mt-4"
+      className="lg:py-[8rem] lg:h-[30rem] md:rounded-b-[80px] w-full rounded-b-[50px] py-[4rem] mt-4"
     >
       <div className="text-white pt-10">
         <div className="md:text-center text-white px-10">
-          <h1 className="md:text-[36px] text-[32px] font-bold">Struggling to sell tickets?</h1>
+          <h1 className="md:text-[36px] text-[32px] font-bold">
+            Struggling to sell tickets?
+          </h1>
           <h6 className="md:text-[18px] text-[16px] font-normal mt-4">
             Create events and sell tickets in minutes-at no cost -with the only
             event platform you need
           </h6>
         </div>
-        <Link 
-        to="/login"
-        className=" no-underline"
-        >
-        <button
-          type="submit"
-          className="
+        {isloggedIn ? (
+          <button
+            className="
+          flex 
+          justify-center 
+          items-center 
+          py-[1.125rem] 
+          px-[5.5rem] 
+          mx-auto mt-[1.5rem] 
+          font-medium 
+          text-base 
+          text-[#FFFFFF] 
+          bg-[#0052F6] 
+          rounded-xl"
+          >
+            Welcome to Teeket !
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsLoggedIn(true)}
+            className="
           lg:text-[18px] 
           lg:mt-[3rem] 
           
@@ -45,9 +63,9 @@ function Hero() {
           rounded-xl
           "
           >
-          Create an event
-        </button>
-        </Link>
+            Create an event
+          </button>
+        )}
       </div>
     </div>
   );
